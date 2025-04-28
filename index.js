5b9e05b46af0f1495d1cded0e906aa27e1e4927e
Duhimcoding <padyaray@gmail.com>
2025-04-28 21:54:18 +0200

    fix: CORS preflight handling

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://english-practice-app-eosin.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
   next();
 });
 
